657862e4e9fad72cef3649d840aceeadf15fc64d
Simon <zj_bupt@sina.com>
2017-09-18 13:50:10 +0800

    用DL思想实现LR模型

--- DeepLearning/LogisticRegression/LR_APP.py
+++ DeepLearning/LogisticRegression/LR_APP.py
@@ -11,77 +11,61 @@ Gather all three functions above into a main model function, in the right order.
 """
 
 import numpy as np
+
+from DeepLearning.utils import sigmoid
 import matplotlib.pyplot as plt
-import h5py
-import scipy
-from PIL import Image
-from scipy import ndimage
-from lr_utils import load_dataset
-# Loading the data (cat/non-cat)
-"""
-- a training set of m_train images labeled as cat (y=1) or non-cat (y=0)
-- a test set of m_test images labeled as cat or non-cat
-- each image is of shape (num_px, num_px, 3) where 3 is for the 3 channels (RGB). 
-Thus, each image is square (height = num_px) and (width = num_px)
-"""
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
-# Example of a picture
-index = 30
-plt.imshow(test_set_x_orig[index])
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
-### START CODE HERE ### (≈ 3 lines of code)
-m_train = train_set_y.shape[1]
-m_test = test_set_y.shape[1]
-num_px = train_set_x_orig.shape[1]
-### END CODE HERE ###
-
-print ("Number of training examples: m_train = " + str(m_train))
-print ("Number of testing examples: m_test = " + str(m_test))
-print ("Height/Width of each image: num_px = " + str(num_px))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_set_x shape: " + str(train_set_x_orig.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x shape: " + str(test_set_x_orig.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-# Reshape the training and test examples
-
-### START CODE HERE ### (≈ 2 lines of code)
-train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
-test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
-### END CODE HERE ###
-
-print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
-print ("train_set_y shape: " + str(train_set_y.shape))
-print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
-print ("test_set_y shape: " + str(test_set_y.shape))
-print ("sanity check after reshaping: " + str(train_set_x_flatten[0:10,1]))
-
-"""
-One common preprocessing step in machine learning is to center and standardize your dataset, 
-meaning that you substract the mean of the whole numpy array from each example, and then divide each example by the standard deviation of the whole numpy array. But for picture datasets, it is simpler and more convenient and works almost as well to 
-just divide every row of the dataset by 255 (the maximum value of a pixel channel).
-"""
-
-train_set_x = train_set_x_flatten/255.
-test_set_x = test_set_x_flatten/255.
-
-
-# GRADED FUNCTION: sigmoid
-
-def sigmoid(z):
-    """
-    Compute the sigmoid of z
-
-    Arguments:
-    z -- A scalar or numpy array of any size.
-
-    Return:
-    s -- sigmoid(z)
-    """
-    s = 1 / (1 + np.exp(-z))
-
-    return s
-
+# import h5py
+# import scipy
+# from PIL import Image
+# from scipy import ndimage
+# from lr_utils import load_dataset
+# # Loading the data (cat/non-cat)
+# """
+# - a training set of m_train images labeled as cat (y=1) or non-cat (y=0)
+# - a test set of m_test images labeled as cat or non-cat
+# - each image is of shape (num_px, num_px, 3) where 3 is for the 3 channels (RGB).
+# Thus, each image is square (height = num_px) and (width = num_px)
+# """
+# train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+# # Example of a picture
+# index = 30
+# plt.imshow(test_set_x_orig[index])
+# print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") +  "' picture.")
+# ### START CODE HERE ### (≈ 3 lines of code)
+# m_train = train_set_y.shape[1]
+# m_test = test_set_y.shape[1]
+# num_px = train_set_x_orig.shape[1]
+# ### END CODE HERE ###
+#
+# print ("Number of training examples: m_train = " + str(m_train))
+# print ("Number of testing examples: m_test = " + str(m_test))
+# print ("Height/Width of each image: num_px = " + str(num_px))
+# print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+# print ("train_set_x shape: " + str(train_set_x_orig.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x shape: " + str(test_set_x_orig.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
+# # Reshape the training and test examples
+#
+# ### START CODE HERE ### (≈ 2 lines of code)
+# train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0],-1).T
+# test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0],-1).T
+# ### END CODE HERE ###
+#
+# print ("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+# print ("train_set_y shape: " + str(train_set_y.shape))
+# print ("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+# print ("test_set_y shape: " + str(test_set_y.shape))
+# print ("sanity check after reshaping: " + str(train_set_x_flatten[0:10,1]))
+#
+# """
+# One common preprocessing step in machine learning is to center and standardize your dataset,
+# meaning that you substract the mean of the whole numpy array from each example, and then divide each example by the standard deviation of the whole numpy array. But for picture datasets, it is simpler and more convenient and works almost as well to
+# just divide every row of the dataset by 255 (the maximum value of a pixel channel).
+# """
+#
+# train_set_x = train_set_x_flatten/255.
+# test_set_x = test_set_x_flatten/255.
 
 # GRADED FUNCTION: initialize_with_zeros
 
@@ -249,7 +233,7 @@ def predict(w, b, X):
 
 # GRADED FUNCTION: model
 
-def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+def logistic_regression_model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
     """
     Builds the logistic regression model by calling the function you've implemented previously
 
@@ -295,48 +279,49 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0
          "num_iterations": num_iterations}
 
     return d
-# Example of a picture that was wrongly classified.
-index = 1
-plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
-print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" +
-       classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
 
-
-# Plot learning curve (with costs)
-costs = np.squeeze(d['costs'])
-plt.plot(costs)
-plt.ylabel('cost')
-plt.xlabel('iterations (per hundreds)')
-plt.title("Learning rate =" + str(d["learning_rate"]))
-plt.show()
-
-"""
-Choice of learning rate
-Reminder: In order for Gradient Descent to work you must choose the learning rate wisely. 
-The learning rate  αα  determines how rapidly we update the parameters. 
-If the learning rate is too large we may "overshoot" the optimal value. 
-Similarly, if it is too small we will need too many iterations to converge to the best values. 
-That's why it is crucial to use a well-tuned learning rate.
-Let's compare the learning curve of our model with several choices of learning rates.
-Run the cell below. This should take about 1 minute. Feel free also to try different values than the three we have initialized the learning_rates variable to contain, and see what happens.
-"""
-learning_rates = [0.01, 0.001, 0.0001]
-models = {}
-for i in learning_rates:
-    print ("learning rate is: " + str(i))
-    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = False)
-    print ('\n' + "-------------------------------------------------------" + '\n')
-
-for i in learning_rates:
-    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
-
-plt.ylabel('cost')
-plt.xlabel('iterations')
-
-legend = plt.legend(loc='upper center', shadow=True)
-frame = legend.get_frame()
-frame.set_facecolor('0.90')
-plt.show()
+# # Example of a picture that was wrongly classified.
+# index = 1
+# plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+# print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" +
+#        classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
+#
+#
+# # Plot learning curve (with costs)
+# costs = np.squeeze(d['costs'])
+# plt.plot(costs)
+# plt.ylabel('cost')
+# plt.xlabel('iterations (per hundreds)')
+# plt.title("Learning rate =" + str(d["learning_rate"]))
+# plt.show()
+#
+# """
+# Choice of learning rate
+# Reminder: In order for Gradient Descent to work you must choose the learning rate wisely.
+# The learning rate  αα  determines how rapidly we update the parameters.
+# If the learning rate is too large we may "overshoot" the optimal value.
+# Similarly, if it is too small we will need too many iterations to converge to the best values.
+# That's why it is crucial to use a well-tuned learning rate.
+# Let's compare the learning curve of our model with several choices of learning rates.
+# Run the cell below. This should take about 1 minute. Feel free also to try different values than the three we have initialized the learning_rates variable to contain, and see what happens.
+# """
+# learning_rates = [0.01, 0.001, 0.0001]
+# models = {}
+# for i in learning_rates:
+#     print ("learning rate is: " + str(i))
+#     models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = False)
+#     print ('\n' + "-------------------------------------------------------" + '\n')
+#
+# for i in learning_rates:
+#     plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+#
+# plt.ylabel('cost')
+# plt.xlabel('iterations')
+#
+# legend = plt.legend(loc='upper center', shadow=True)
+# frame = legend.get_frame()
+# frame.set_facecolor('0.90')
+# plt.show()
 
 
 
